d60307b9752c0e11ee11976b03e565e3f91cd081
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-02-12 19:07:41 -0500

    Create barrier.h

---
+++ SSort/barrier.h
@@ -0,0 +1,19 @@
+#ifndef BARRIER_H
+#define BARRIER_H
+
+#include <pthread.h>
+
+typedef struct barrier {
+    // TODO: Need some synchronization stuff.
+    pthread_mutex_t  mutex;
+    pthread_cond_t  barrier;
+    int count;
+    int seen;
+} barrier;
+
+barrier* make_barrier(int nn);
+void barrier_wait(barrier* bb);
+void free_barrier(barrier* bb);
+
+
+#endif
